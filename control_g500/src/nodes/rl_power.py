302046e8e3103ef2aec9f09492c50c5f8a8af952
acarrera <acarrera@eia.udg.edu>
2012-04-06 18:59:08 +0200

    Afegit el core de l'algorisme

--- control_g500/src/nodes/rl_power.py
+++ control_g500/src/nodes/rl_power.py
@@ -31,10 +31,54 @@ class RlPoWER( RlAlgorithm ) :
 
 
     
-    def rl_main(self):
-        roslib.loginfo("S'ha engeta ")
-        roslib.loginfo("El nomebre de parametres es: %s", self.n_params)
+#    def rl_main(self):
+#        rospy.loginfo("S'ha engeta ")
+#        rospy.loginfo("El nomebre de parametres es: %s", self.n_params)
 
+
+    def rl_function(self,trials,trial):
+        """PoWER Implementation"""
+        if trial == 0 : # do initialization
+            new_policy = self.initial_params
+            
+        else :
+            #build the lookup table for the importance sampling
+            s_Return = hstack( ( array([trials.reward[0:trial-1]]).T ,  array([range(trial-1)]).T  )  )
+            print s_Return 
+            index = argsort(s_Return[:,0])
+            s_Return = s_Return[index,:] # all previous trials sortide by reward ASC
+
+            #update the policy parameters
+            param_nom = zeros(self.n_params, 1)
+            param_dnom = 0
+        
+            #calculate the expectations (the normalization is taken care of by the division)
+            #as importance sampling we take the 10 best rollouts
+
+            for i in xrange( min(self.importance_sampling_top_count, trial-1) ) :
+                j = s_Return[s_Return.shape[0]-i-1, 1]# get the trial number for the best sigma trials 
+                #calculate the exploration with respect to the current (i.e. last trial) parameters
+                temp_explore = trials.policy[:,j] - trials[:,trial-1]
+                #always have the same exploration variance,
+                #and assume that always only one basis functions is active we get these simple sums
+                param_nom = param_nom + temp_explore*trials.reward[j]
+                param_dnom = param_dnom + trials.reward[j]
+
+            #update the parameters
+            new_policy = trials.policy[:,trial-1] + param_nom/(param_dnom+1e-10)
+            #add noise using the decayed variance
+            variance = self.initial_variance * self.variance_decay**(trial-1); 
+            #start decaying the noise from 3nd trial
+            new_policy = new_policy + ((variance**0.5)*random.rand(self.n_params,1)) 
+
+        return new_policy
+
+
+    def retunrOfTrial(self,policy,n_steps):
+        return ( 10000 - (policy[0]**2 + policy[1]**2) )
+    
+    
+    
 
 
 
@@ -43,6 +87,6 @@ if __name__ == '__main__':
         rospy.init_node('rl_PoWER')
         rl_PoWER = RlPoWER(rospy.get_name())
         
-        #self.rl_main()
+        rl_PoWER.rl_main()
        
     except rospy.ROSInterruptException: pass
